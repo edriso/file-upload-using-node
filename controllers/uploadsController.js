30b9615919491a4865a787318d76d3b1468ca834
edriso <edriso.dev@gmail.com>
2023-02-25 09:59:47 +0200

    check if image exists, its format, and its size

--- controllers/uploadsController.js
+++ controllers/uploadsController.js
@@ -1,8 +1,30 @@
 const path = require('path');
 const { StatusCodes } = require('http-status-codes');
+const CustomError = require('../errors');
 
 const uploadProductImage = async (req, res) => {
+  // check if user uploaded a file
+  if (!req.files) {
+    throw new CustomError.BadRequestError('No File Uploaded');
+  }
+
   const productImage = req.files.image;
+
+  // check format
+  if (!productImage.mimetype.startsWith('image')) {
+    throw new CustomError.BadRequestError('Please Upload an Image');
+  }
+
+  // check size
+  const maxSize = 1024 * 1024;
+  if (productImage.size > maxSize) {
+    throw new CustomError.BadRequestError(
+      `Please upload image smaller than ${(maxSize / 1024 / 1024).toFixed(
+        2
+      )} MB`
+    );
+  }
+
   const imagePath = path.join(
     __dirname,
     `../public/uploads/${productImage.name}`
