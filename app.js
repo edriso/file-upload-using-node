f128cfe42cbfb8c1cc44cd17779144de0c7c4ebc
edriso <edriso.dev@gmail.com>
2023-02-25 11:56:31 +0200

    remove unnecessary comment

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ const app = express();
 
 app.use(express.static('./public'));
 app.use(express.json());
-// app.use(express.json({ limit: '50mb' }));
+
 app.use(
   fileUpload({
     useTempFiles: true,
